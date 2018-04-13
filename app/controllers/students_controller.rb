8ee7a5538d583a6ac53e59f8cdec192f1a7f19e5
Brendan O'Dea <brendan.ode4@gmail.com>
2018-04-13 23:41:37 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,7 +16,7 @@ class StudentsController < ApplicationController
     else
       @student.active = true
     end
-    activate_student_path(@student)
+    student_path(@student)
   end
   
   private

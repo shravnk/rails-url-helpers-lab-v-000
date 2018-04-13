0386671374dd1b2f126e465a4b7b0b7602255c7e
Brendan O'Dea <brendan.ode4@gmail.com>
2018-04-13 23:20:29 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,16 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :activate]
   
   def index
     @students = Student.all
   end
 
   def show
-    set_student
+    
   end
 
   def activate
-    set_student
+    
     if @student.active == true
       @student.active = false
     else
